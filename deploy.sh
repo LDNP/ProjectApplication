ff70bff5c7ada22a7378b5072db74340d3e775e4
lisadavis552-Git <lisadavis8479@gmail.com>
2025-04-02 15:22:18 +0100

    changes to deploy.sh

--- deploy.sh
+++ deploy.sh
@@ -16,8 +16,8 @@ pm2 stop project_app || true
 npm install
 
 # Recreate certs (from environment variables set in CircleCI)
-echo "$PRIVATE_KEY" > certs/privatekey.pem
-echo "$SERVER" > certs/server.crt
+echo $PRIVATE_KEY > privatekey.pem 
+echo $SERVER > server.crt 
 
 # Build the frontend (only if needed)
 npm run build
